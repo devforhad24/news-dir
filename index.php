a62ff5b94022b27c806b758db97f61d0523fdf2d
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-31 16:34:35 +0600

    HTML layout done

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <div class="mainBody">
+        <div class="topBar">
+            <h1>NewsDir</h1>
+        </div>
+        <div class="menuBar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Bangladesh</a></li>
+                <li><a href="#">India</a></li>
+                <li><a href="#">America</a></li>
+            </ul>
+        </div>
+        <div class="contentBody">
+            <div class="contentBodyLeft">
+                <div class="contentContainer">
+                    <div class="logoContainer">
+                        <img src="images/Prothom-Alo-logo.jpg" alt="prothomalo">
+                        <h3>Prothom Alo</h3>
+                    </div>
+                    <div class="logoContainer">
+                        <img src="images/timesofindia.jpg" alt="prothomalo">
+                        <h3>Times of India</h3>
+                    </div>
+                    <div class="logoContainer">
+                        <img src="images/bbc_news_logo.png" alt="prothomalo">
+                        <h3>BBC News</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="contentBodyRight">
+                <div class="contentBodyRightOne">
+                    contebodyright1
+                </div>
+                <div class="contentBodyRightTwo">
+                    contentbodyrihgt2
+                </div>
+            </div>
+        </div>
+        <div class="copyrightBar">
+            copyright
+        </div>
+    </div>
+
+</body>
+</html>
